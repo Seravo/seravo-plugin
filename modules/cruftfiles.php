c9fff6b0c7630c0ac7d2b938618b7be1793e5325
Leo Toikka <leo@seravo.fi>
2017-11-16 19:54:46 +0200

    Move cruft files JS to separate file and add CSS file
    It is more convenient, if all the needed javascript is in a separate file
    and this file is then enqueued via the provided enqueue functionality of
    WordPress.

--- modules/cruftfiles.php
+++ modules/cruftfiles.php
@@ -12,6 +12,7 @@ if ( ! class_exists('Cruftfiles') ) {
 
     public static function load() {
       add_action( 'admin_menu', array( __CLASS__, 'register_cruftfiles_page' ) );
+      add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_cruftfiles_scripts' ) );
       add_action('wp_ajax_seravo_cruftfiles', function() {
           require_once(dirname( __FILE__ ) . '/../lib/cruftfiles-ajax.php');
           wp_die();
@@ -38,6 +39,23 @@ if ( ! class_exists('Cruftfiles') ) {
       wp_die();
     }
 
+    public static function enqueue_cruftfiles_scripts( $hook ) {
+      wp_register_style( 'seravo_cruftfiles', plugin_dir_url( __DIR__ ) . '/style/cruftfiles.css' );
+      wp_register_script( 'seravo_cruftfiles', plugin_dir_url( __DIR__ ) . '/js/cruftfiles.js' );
+
+      if ( $hook === 'tools_page_cruftfiles_page' ) {
+        wp_enqueue_style( 'seravo_cruftfiles' );
+        wp_enqueue_script( 'seravo_cruftfiles' );
+
+        // Localize the javascript file.
+        $loc_translation = array(
+            'no_data' => __( 'No data returned for section.', 'seravo' ),
+             'confirm' => __( 'Are you sure you want to proceed? Deleted files can not be recovered.', 'seravo' ),
+             'fail' => __( 'Failed to load. Please try again.', 'seravo' ),
+        );
+        wp_localize_script( 'seravo_cruftfiles', 'seravo_cruftfiles_loc', $loc_translation );
+      }
+    }
   }
 
   cruftfiles::load();
