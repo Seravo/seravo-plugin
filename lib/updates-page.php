62840494ffe15a975e8ec92b3225f6e38270099c
Leo Toikka <leo@seravo.fi>
2017-12-04 19:44:00 +0200

    Include technical contact email field to the updates page
    Added a field where the customer can set site-specific technical contact
    email addresses, which are then updated via SWD API to the site info.

--- lib/updates-page.php
+++ lib/updates-page.php
@@ -15,8 +15,8 @@ $site_info = Seravo\Updates::seravo_admin_get_site_info();
   <li><?php _e('Latest successful update', 'seravo')?>: <?php echo $site_info['update_success']; ?></li>
 </ul>
 
-
 <h2><?php _e('Opt-out form updates by Seravo', 'seravo') ?></h2>
+
 <?php
 if ( $site_info['seravo_updates'] === true ) {
   $checked = 'checked="checked"';
@@ -24,15 +24,28 @@ if ( $site_info['seravo_updates'] === true ) {
   $checked = '';
 }
 
-// @TODO: Submit a nonce with the form if the amount of fields and inputs grows to protect users from XSS attacks.
+$contact_emails = array();
+if ( isset($site_info['contact_emails']) ) {
+  $contact_emails = $site_info['contact_emails'];
+}
+
 ?>
 
   <p><?php _e('Seravo\'s upkeep service includes that your WordPress site is kept up-to-date with quick security updates and regular tested updates of both WordPress core and plugins. If you want full control of updates yourself, you can opt-out from Seravo updates.', 'seravo') ?></p>
 
-  <form name="toggle_seravo_updates" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
-    <?php wp_nonce_field( 'toggle-seravo-updates-on-or-off' ); ?>
+  <form name="seravo_updates_form" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
+    <?php wp_nonce_field( 'seravo-updates-nonce' ); ?>
     <input type="hidden" name="action" value="toggle_seravo_updates">
-    <input id="seravo_updates" name="seravo_updates" type="checkbox" <?php echo $checked; ?>> <?php _e('Seravo updates enabled', 'seravo') ?><br><br>
-    <input type="submit" value="<?php _e('Save settings', 'seravo') ?>">
+    <input id="seravo_updates" name="seravo_updates" type="checkbox" <?php echo $checked; ?>> <?php _e('Seravo updates enabled', 'seravo') ?><br>
+
+    <h2><?php _e('Technical contact email addresses', 'seravo') ?></h2>
+    <p><?php _e('Seravo may send automatic notifications about site errors and failed updates to these addresses. Please separate multiple email addresses with commas.', 'seravo') ?></p>
+    <input name="technical_contacts" type="email" multiple size="30" placeholder="<?php _e('example@example.com', 'seravo') ?>" value="<?php if ( ! empty($contact_emails) ) : echo implode(', ', $contact_emails); endif; ?>">
+
+    <p><small class="seravo-developer-letter-hint">
+      <?php echo sprintf( __('P.S. Subscribe to Seravo\'s %sNewsletter for WordPress Developers%s to get up-to-date information about our newest features.', 'seravo'), '<a href="https://seravo.com/newsletter-for-wordpress-developers/">', '</a>'); ?>
+    </small></p>
+
+    <input type="submit" class="button button-primary" value="<?php _e('Save settings', 'seravo') ?>">
   </form>
 </div>
