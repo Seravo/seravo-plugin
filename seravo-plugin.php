abb7626492bcf2b3788d76cd985e1229bf5e5050
Otto Kekäläinen <otto@seravo.fi>
2017-12-16 22:17:39 +0200

    Bump version to 1.7.0

--- seravo-plugin.php
+++ seravo-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Seravo Plugin
- * Version: 1.6.2
+ * Version: 1.7.0
  * Plugin URI: https://github.com/Seravo/seravo-plugin
  * Description: Enhances WordPress with Seravo.com specific features and integrations.
  * Author: Seravo Oy
