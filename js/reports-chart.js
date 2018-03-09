f2da1c4eb08b4ef4e9891d5c7c435f64719adfb4
Susanna Lepola <suskaari@hotmail.com>
2018-03-09 18:57:07 +0200

    Update layout to wordpress layout
    Add accordion to postboxes
    
    Change position of postboxes
    
    Show disk-usage data in pie chart
    
    Generate disk-usage pie chart colors
    
    Add scrollbar in postboxes
    
    Fix code intend and improve comments
    
    Specify width of the HTTP-request bar in percentage
    
    Show only icon on mobile in HTTP-request "Show more" -button

---
+++ js/reports-chart.js
@@ -0,0 +1,78 @@
+function generateChart(JSONdata) {
+  var data = [];
+  var labels = [];
+  var background = [];
+  // Create chart colors of string
+  Object.keys(JSONdata).forEach(folder => {
+    data.push(JSONdata[folder].percentage);
+    labels.push(folder + ' - ' + JSONdata[folder].human);
+    background.push((new ColorHash({ saturation: [0.9, 0, 1] })).hex(folder));
+  });
+  // Generate chart
+  var ctx = document.getElementById('pie_chart');
+  var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+          backgroundColor: background,
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      tooltips: {
+        callbacks: {
+          label: function (tooltipItem, data) {
+            var label = data.labels[tooltipItem.index] || '';
+            if (label) {
+              label += ': ';
+            }
+            label += parseFloat(data.datasets[0].data[tooltipItem.index]).toFixed(2) + '%';
+            return label;
+          }
+        }
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          fontColor: 'black',
+          boxWidth: 10
+        }
+      }
+    }
+  });
+  // Modifies chart legend cursor to pointer
+  Chart.plugins.register({
+    afterEvent: function (chartInstance, chartEvent) {
+      var legend = chartInstance.legend;
+      var canvas = chartInstance.chart.canvas;
+      var x = chartEvent.x;
+      var y = chartEvent.y;
+      var cursorStyle = 'default';
+      if (
+        x <= legend.right
+        && x >= legend.left
+        && y <= legend.bottom
+        && y >= legend.top
+      ) {
+        for (var i = 0; i < legend.legendHitBoxes.length; ++i) {
+          var box = legend.legendHitBoxes[i];
+          if (
+            x <= box.left + box.width
+            && x >= box.left
+            && y <= box.top + box.height
+            && y >= box.top
+          ) {
+            cursorStyle = 'pointer';
+            break;
+          }
+        }
+      }
+      canvas.style.cursor = cursorStyle;
+    }
+  });
+}
