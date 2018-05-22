72ca9d72322ea738f18992f5bb92a3ef51de28c9
Susanna Lepola <suskaari@hotmail.com>
2018-05-22 09:16:02 +0300

    Fix log-viewer.js file indentation

--- js/log-viewer.js
+++ js/log-viewer.js
@@ -1,41 +1,42 @@
 'use strict';
 
 jQuery(document).ready(function($) {
-    $(window).on('load', function() {
-        // auto-scroll to bottom of log viewers on page load
-        $('.log-table-view').each(function() {
-        $(this).scrollTop($('table', this).height())
+  $(window).on('load', function() {
+    // auto-scroll to bottom of log viewers on page load
+    $('.log-table-view').each(function() {
+      $(this).scrollTop($('table', this).height())
     });
-});
+  });
 
-$('.log-table-view').on('scroll', function(e) {
+  $('.log-table-view').on('scroll', function(e) {
     var $this = $(this);
     if ( 0 == $this.scrollTop() && 0 == $this.find('.overlay').length ) {
-        var $overlay = $('<div class="overlay"><div>');
-        $this.append( $overlay );
-        // load more lines
-        var offset = $('td', this).length;
-        var payload = {
-            'action': 'fetch_log_rows',
-            'logfile': $this.data('logfile'),
-            'offset': $('td', this).length,
-            'regex': $this.data('regex'),
-            'cutoff_bytes': $this.data('logbytes')
-        };
+      var $overlay = $('<div class="overlay"><div>');
+      $this.append( $overlay );
+      // load more lines
+      var offset = $('td', this).length;
+      var payload = {
+        'action': 'fetch_log_rows',
+        'logfile': $this.data('logfile'),
+        'offset': $('td', this).length,
+        'regex': $this.data('regex'),
+        'cutoff_bytes': $this.data('logbytes')
+      };
 
-        $.post(window.ajaxurl, payload, function(response) {
-            var oldheight = $('table', $this).height();
-            $('tbody', $this).prepend(response);
-            $this.scrollTop($('table', $this).height() - oldheight);
-            $overlay.remove();
-        });
+      $.post(window.ajaxurl, payload, function(response) {
+        var oldheight = $('table', $this).height();
+        $('tbody', $this).prepend(response);
+        $this.scrollTop($('table', $this).height() - oldheight);
+        $overlay.remove();
+      });
     }
-});
-// Script for showing clicked row on log-view-active box
-jQuery(".logrow").click(function () {
+  });
+
+  // Script for showing clicked row on log-view-active box
+  jQuery(".logrow").click(function () {
     var $row = jQuery(this).closest("td"); // Find the row
     var $text = $row.find(".logrow").text(); // Find the text
 
     jQuery('.log-view-active').text($text);
-});
+  });
 });
