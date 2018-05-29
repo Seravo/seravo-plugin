d7973401cca04999fbbdb3496d6cae4085e51c66
Mikko Ala-Fossi <mikko@seravo.fi>
2018-05-29 16:59:17 +0300

    Add shadow reset functionality with basic feedback

--- modules/shadows.php
+++ modules/shadows.php
@@ -10,6 +10,7 @@
 namespace Seravo;
 
 require_once dirname( __FILE__ ) . '/../lib/helpers.php';
+require_once dirname( __FILE__ ) . '/../lib/shadows-ajax.php';
 
 if ( ! class_exists('Shadows') ) {
   class Shadows {
@@ -18,17 +19,19 @@ if ( ! class_exists('Shadows') ) {
       // Load only in production
       if ( Helpers::is_production() ) {
         add_action( 'admin_menu', array( __CLASS__, 'register_shadows_page' ) );
+        wp_register_script( 'seravo_shadows', plugin_dir_url( __DIR__ ) . '/js/shadows.js' );
+        wp_enqueue_script( 'seravo_shadows' );
+        add_action( 'wp_ajax_seravo_reset_shadow' , 'seravo_reset_shadow' );
       }
     }
-
     public static function register_shadows_page() {
-      add_submenu_page( 'tools.php', __('Shadows', 'seravo'), __('Shadows', 'seravo'), 'manage_options', 'shadows_page', array( __CLASS__, 'load_shadows_page' ) );
+      add_submenu_page( 'tools.php', __('Shadows', 'seravo'),
+      __('Shadows', 'seravo'), 'manage_options', 'shadows_page',
+      array( __CLASS__, 'load_shadows_page' ) );
     }
-
     public static function load_shadows_page() {
       require_once dirname( __FILE__ ) . '/../lib/shadows-page.php';
     }
   }
-
   Shadows::load();
 }
