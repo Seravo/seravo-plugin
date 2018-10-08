c32f0dd0e32cdae702b721138ce18c18d25ddf0c
Leo Toikka <leo@seravo.fi>
2018-10-08 09:56:59 +0300

    Include existing query params when redirecting after purging the cache
    Prior to this change, all query parameters where discarded and only the
    query parameter 'seravo_purge_success' was left. As a result, e.g. when
    purging the cache on /wp-admin/tools.php?page=backups_page, the redirection
    would cause the URL to be /wp-admin/tools.php?seravo_purge_success=true,
    which would mean that the user was redirected away from the backups page
    to the initial "Available Tools" page.
    
    This changes includes extracting the query parameter key=value pairs from
    the current URL and reinserting them with the 'seravo_purge_success' parameter
    value included.

--- js/purge-cache.js
+++ js/purge-cache.js
@@ -13,11 +13,20 @@ jQuery(document).ready(function($) {
       action: 'seravo_purge_cache',
       nonce: seravo_purge_cache_loc.seravo_purge_cache_nonce,
     }, function(response) {
-      var query_object = {
-        'seravo_purge_success': response.success,
-      };
-      window.location = window.location.pathname + '?' + $.param(query_object);
-    });
+      var query_params = {};
+      // Query string without the initial question mark "?"
+      var query_string = location.search.substring(1);
+      // Regex for matching query string key=value pairs
+      var query_regex = /([^&=]+)=([^&]*)/g;
+      var match;
 
+      // Create a map with the already existing query string parameters
+      while (match = query_regex.exec(query_string)) {
+        query_params[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
+      }
+
+      // Add or update the Seravo purge cache success parameter
+      query_params['seravo_purge_success'] = response.success;
+      location.search = $.param(query_params);
   });
 });
