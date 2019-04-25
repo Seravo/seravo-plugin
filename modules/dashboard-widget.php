54729d57dc13ad5f3e32d8f243893c4e3cb70b57
Samuli Lehtonen <samuli@seravo.fi>
2019-04-25 13:16:39 +0300

    WIP: Fix code style in dashboard widget

--- modules/dashboard-widget.php
+++ modules/dashboard-widget.php
@@ -6,7 +6,7 @@ if ( ! defined('ABSPATH') ) {
 
 if ( ! class_exists('Dashboard_Widget') ) {
   class Dashboard_Widget {
-    
+
     function load() {
       add_action( 'wp_dashboard_setup', array( __CLASS__, 'seravo_plugin_custom_dashboard_widget') );
     }
