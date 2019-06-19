4552765ba740e8bd765c3a53c1a9040f5e88f7cf
Joosua Koskinen <joosua@seravo.fi>
2019-06-19 13:23:15 +0300

    Improve shadow domain support
    This improves the support of custom domain names for shadows.
    As shadows don't have access to domain info of production container,
    it's not really posssible to have full bulletproof implementation.
    
    The old system functions the following way:
    - Clear seravo_shadow cookie
      -> If set on different domain, user is stuck
    
    The new fallback system:
    - Check if seravo_shadow cookie is set
      -> If so, clear it
    - If not, check if seravo_production cookie is set
      -> If so, redirect to it
    - If not, check if subdomain is used (Doesn't work with TLD on second level eg. co.uk)
      -> If so, parse subdomain out and redirect to it
    - Redirect to 'DEFAULT_DOMAIN' root path and trust that it redirects to WP_HOME
    
    Shadows with domains will not set the seravo_shadow cookie but they still can be
    accessed throught it (this doesn't break anything / backwards compatible).
    
    Easiest ways to get stuck now are:
    - Set seravo_shadow cookie manually (if you know how to set it, you know how to clear it)
    - All of the following at the same time (really unlikely):
      * Access without instance-switcher (seravo_production not set)
      * Use shadow without subdomain
      * Redirection to WP_HOME is broken on production site

--- js/instance-switcher.js
+++ js/instance-switcher.js
@@ -1,30 +1,82 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Select all links in shadow drop-down
-  var links = document.querySelectorAll('#wp-admin-bar-instance-switcher li > a');
+  // Select all links for entering shadow in admin bar dropdown
+  var shadow_links = jQuery('#wp-admin-bar-instance-switcher li.shadow-link > a');
+  // Select all links for exiting shadow (dropdown or banner)
+  var exit_links = jQuery('li.shadow-exit > a, a.shadow-exit');
 
-  var listener = function(e){
-    // Match all strings #something in the link targets.
-    // Typically #exit or #abc123 (shadow ID).
-    var instance = e.target.getAttribute('href').match(/#([a-z0-9]+)/)[1];
-    if (instance === 'exit') {
-      // If exit selected, clear cookies.
-      e.preventDefault();
+  // Listener for entering shadow
+  var shadow_listener = function(e) {
+    e.preventDefault();
+
+    var new_location = location.href;
+    var target = jQuery(this).attr('href');
+    if (target.startsWith('#')) {
+      // Then access shadow with cookies
+      // Match all strings eg. #abc123 (shadow ID)
+      var instance = target.match(/#([a-z0-9]+)/);
+      // instance[0] = #abc123, instance[1] = abc123
+      if (instance && instance[1] && instance[1].length === 6) {
+        // Set the cookies, 43200 seconds is 12 hours
+        document.cookie = "wpp_shadow=" + instance[1] + "; Max-Age=43200; Path=/";
+        document.cookie = "seravo_shadow=" + instance[1] + "; Max-Age=43200; Path=/";
+        // Clear potential old shadow query string
+        new_location = new_location.replace(/[a-z]+_shadow=[a-z0-9]+/, '');
+      }
+    } else  {
+      // Else access shadow with domain
+      var current_host = location.protocol + '//' + location.hostname;
+      new_location = new_location.replace(current_host, target);
+      // Clean away potential old seravo_production param.
+      new_location = new_location.replace(/(\?|\b)seravo_production=.*?(?=&|$)/, '');
+      // Add seravo_production param with current hostname
+      new_location = new_location.replace(/#.*/, '');
+      new_location += new_location.indexOf('?') != -1 ? '&' : '?';
+      new_location += 'seravo_production=' + location.hostname;
+    }
+    // Reload / redirect page
+    location.href = new_location;
+  }
+
+  // Listener for exiting shadow
+  var exit_listener = function(e) {
+    e.preventDefault();
+
+    var new_location = location.href;
+    var target = jQuery(this).attr('href');
+    if (target === '#exit') {
+      // Then using cookies to access shadow, clear them
       document.cookie = "wpp_shadow=; Max-Age=0; Path=/";
       document.cookie = "seravo_shadow=; Max-Age=0; Path=/";
-    } else if (instance.length === 6) {
-      // If shadow selected, set cookies.
-      e.preventDefault();
-      // 43200 seconds is 12 hours
-      document.cookie = "wpp_shadow=" + instance + "; Max-Age=43200; Path=/";
-      document.cookie = "seravo_shadow=" + instance + "; Max-Age=43200; Path=/";
+      // Clear potential old shadow query string
+      new_location = new_location.replace(/[a-z]+_shadow=[a-z0-9]+/, '');
+    } else {
+      // Else using domain
+      if (target.endsWith('/')) {
+        // Only use hostname so redirecting works
+        // Used with DEFAULT_DOMAIN
+        new_location = target;
+      } else {
+        // Otherwise keep protocol, path and query string
+        // Clean away potential old seravo_production param.
+        new_location = new_location.replace(/(\?|\b)seravo_production=.*?(?=&|$)/, '');
+        new_location = new_location.replace(location.protocol + '//' + location.hostname, target);
+      }
     }
 
-    // Clean away potential old shadow query strings and reload page
-    newloc = location.href.replace(/[a-z]+_shadow=[a-z0-9]+/, '')
-    location.href = newloc;
-  };
-  var limit = links.length;
-  for (var i = 0; i < limit; i++) {
-    links[i].addEventListener('click', listener);
+    // Reload / redirect page
+    location.href = new_location;
   }
+
+  // Add right listener for the links found
+
+  var limit = shadow_links.length;
+  for (var i = 0; i < limit; i++) {
+    shadow_links[i].addEventListener('click', shadow_listener);
+  }
+
+  limit = exit_links.length;
+  for (var i = 0; i < limit; i++) {
+    exit_links[i].addEventListener('click', exit_listener);
+  }
+
 });
