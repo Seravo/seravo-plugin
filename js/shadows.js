927cb818a82b6a96bfd35424124ec6a2aefdae72
Joosua Koskinen <joosua@seravo.fi>
2019-07-02 11:12:26 +0300

    Redesign shadows postbox
    Changes the width of shadows postbox from 100% to default to support
    multiple postboxes on the same page (upcoming centralization changes).
    
    To be able to fit all the functionality in a smaller postbox,
    dropdown is used to select the shadow (like in mails-page) instead of showing all of them
    in a table.
    
    New design also has better shadow domain support.

--- js/shadows.js
+++ js/shadows.js
@@ -2,21 +2,64 @@
 'use strict';
 
 jQuery(document).ready(function($) {
-  $('.shadow-reset').click(function(event) {
+  $('#shadow-selector').change(function() {
+    var $shadow = $("[data-shadow=" + $(this).val() + "]");
+    if ($shadow) {
+      $('.shadow-row').addClass('shadow-hidden');
+      $shadow.removeClass('shadow-hidden');
+      $('#shadow-reset').prop('disabled', false);
+      $('#shadow-reset-status').html('');
+      update_data_gui();
+    }
+  });
+  $('.action-link').click(function(event) {
+    event.preventDefault();
+    if (! $(this).hasClass('closed')) {
+      $(this).addClass('closed');
+      $('#shadow-data-actions').addClass('shadow-hidden');
+    } else {
+      $(this).removeClass('closed');
+      $('#shadow-data-actions').removeClass('shadow-hidden');
+      update_data_gui();
+    }
+  });
+
+  function update_data_gui() {
+    var instance = $('#shadow-selector').val();
+    $('#shadow-reset-instance').html(instance);
+    var production_domain = $('input[name=shadow-reset-production]').val();
+    var shadow_domain = $('[data-shadow=' + instance + ']').attr('data-domain');
+    if (shadow_domain) {
+      $('input[name=shadow-reset-sr-from]').val(production_domain);
+      $('input[name=shadow-reset-sr-to]').val('://' + shadow_domain);
+      $('#shadow-reset-sr-alert').removeClass('shadow-hidden');
+      $('#shadow-reset-nosr-alert').addClass('shadow-hidden');
+    } else {
+      $('input[name=shadow-reset-sr-from]').val('');
+      $('input[name=shadow-reset-sr-to]').val('');
+      $('#shadow-reset-sr-alert').addClass('shadow-hidden');
+      $('#shadow-reset-nosr-alert').removeClass('shadow-hidden');
+    }
+    $('#shadow-reset').prop('disabled', false);
+    $('#shadow-reset-status').html('');
+  }
+
+  $('#shadow-reset').click(function(event) {
     var is_user_sure = confirm(seravo_shadows_loc.confirm);
     if ( ! is_user_sure) {
       return;
     }
-    seravo_ajax_reset_shadow($(this).attr("data-shadow-name"),
+    seravo_ajax_reset_shadow($('#shadow-selector').val(),
       function( status ){
         if ( status == 'progress' ) {
           event.target.disabled = true;
+          $('#shadow-reset-status').html("<img src=\"/wp-admin/images/spinner.gif\">");
         } else if ( status == 'success' ) {
-          event.target.innerHTML = seravo_shadows_loc.success;
+          $('#shadow-reset-status').html(seravo_shadows_loc.success);
         } else if ( status == 'failure' ) {
-          event.target.innerHTML = seravo_shadows_loc.failure;
+          $('#shadow-reset-status').html(seravo_shadows_loc.failure);
         } else {
-          event.target.innerHTML = seravo_shadows_loc.error;
+          $('#shadow-reset-status').html(seravo_shadows_loc.error);
         }
       });
   });
@@ -26,8 +69,9 @@ jQuery(document).ready(function($) {
     $.post(
       seravo_shadows_loc.ajaxurl,
       { type: 'POST',
-        'action': 'seravo_reset_shadow',
-        'resetshadow': shadow,
+        'action': 'seravo_ajax_shadows',
+        'section': 'seravo_reset_shadow',
+        'shadow': shadow,
         'nonce': seravo_shadows_loc.ajax_nonce, },
         function( rawData ) {
           var data = JSON.parse(rawData);
