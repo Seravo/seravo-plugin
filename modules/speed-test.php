6ed54ae507e2cf082ce6f52179a1247b38e74596
Viljami Pietarila <viljami@seravo.com>
2020-07-08 11:08:20 +0300

    Add GUI for speed test
    Under Tools -> Site Status there is now block for Speed Test to run
    tests to different urls within the wordpress site. The test runs multiple
    curl commands from within the server to see the speed of generating the
    html from php. The tests are run with both with and without cache enabled.
    
    There is button in the wordpress admin bar to test the speed of current page
    using the above mentioned GUI.

---
+++ modules/speed-test.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Description: Adds Speed Test button to the WP admin bar
+ */
+
+namespace Seravo;
+
+// Deny direct access to this file
+if ( ! defined('ABSPATH') ) {
+  die('Access denied!');
+}
+
+if ( ! class_exists('Speed_Test') ) {
+  class Speed_Test {
+
+    public static function load() {
+      add_action('wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ));
+      add_action('admin_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ));
+      add_action('admin_bar_menu', array( __CLASS__, 'speed_test_button' ), 1001);
+    }
+
+    // Add speed test button to WP Admin Bar
+    public static function speed_test_button( $admin_bar ) {
+      $target_location = ltrim($_SERVER['REQUEST_URI'], '/');
+
+      if ( substr($target_location, 0, 9) === 'wp-admin/' ) {
+        $admin_bar->add_menu(
+          array(
+            'id'    => 'speed-test',
+            'title' => '<span class="ab-icon seravo-speed-test-icon"></span><span class="ab-label seravo-speed-test-text" title="' .
+              sprintf(__('Test the speed of the current page. Note: This does not function within wp-admin!', 'seravo')) . '">' . __('Speed Test', 'seravo') . '</span>',
+          )
+        );
+      } else {
+        $url = get_home_url() . '/wp-admin/tools.php?page=site_status_page&speed_test_target=' . $target_location;
+        $admin_bar->add_menu(
+          array(
+            'id'    => 'speed-test',
+            'title' => '<span class="ab-icon seravo-speed-test-icon"></span><span class="ab-label seravo-speed-test-text" title="' .
+            sprintf(__('Test the speed of the current page.', 'seravo')) . '">' . __('Speed Test', 'seravo') . '</span>',
+            'href'  => $url,
+          )
+        );
+      }
+    }
+
+    /**
+     * Load required scripts and styles for this module
+     */
+    public static function enqueue_scripts() {
+      wp_enqueue_style('seravo_speed_test', plugins_url('../style/speed-test.css', __FILE__), null, Helpers::seravo_plugin_version(), 'all');
+    }
+  }
+  /* Caching happens in general only in production */
+  if ( Helpers::is_production() ) {
+    Speed_Test::load();
+  }
+}
