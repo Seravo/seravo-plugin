ae5345a3263305157d9fcbab66f538c663151778
Viljami Pietarila <viljami@seravo.com>
2020-07-21 10:05:43 +0300

    Change the PHP update instruction urls
    The PHP update instructions now point to the host's update
    instruction pages instead of default ones.
    
    Change PHP update admin notice to take in account user locale
    for the instruction url.
    
    Closes #388

--- modules/check-php-version.php
+++ modules/check-php-version.php
@@ -20,7 +20,7 @@ if ( ! class_exists('CheckPHPVersion') ) {
     public static function load() {
 
       add_action('admin_notices', array( __CLASS__, '_seravo_check_php_version' ));
-
+      add_filter('wp_update_php_url', array( __CLASS__, '_seravo_update_php_url' ));
     }
 
     public static function _seravo_check_php_version() {
@@ -43,8 +43,12 @@ if ( ! class_exists('CheckPHPVersion') ) {
 
     }
 
-    public static function _seravo_show_php_recommendation( $recommended_version ) {
+    public static function _seravo_update_php_url() {
+      return __('https://help.seravo.com/article/41-set-your-site-to-use-newest-php-version', 'seravo');
+    }
 
+    public static function _seravo_show_php_recommendation( $recommended_version ) {
+      $url = self::_seravo_update_php_url();
       ?>
       <div class="notice notice-info">
       <p>
@@ -53,9 +57,10 @@ if ( ! class_exists('CheckPHPVersion') ) {
       // The line below is very long, but PHPCS standards requires translation
       // strings to be one one line
       printf(
-        // translators: %1$s: current php version, %2$s: recommended php version
-        __('PHP %s is available but not used on this site. Developers might want to <a href="tools.php?page=upkeep_page">upgrade the latest PHP version</a> for faster performance and new features. Read more about <a target="_blank" href="https://help.seravo.com/article/41-set-your-site-to-use-newest-php-version">PHP version upgrades</a>.', 'seravo'),
-        $recommended_version
+        // translators: %1$s: recommended php version, %2$s: url to the update page
+        __('PHP %1$s is available but not used on this site. Developers might want to <a href="tools.php?page=upkeep_page">upgrade the latest PHP version</a> for faster performance and new features. Read more about <a target="_blank" href="%2$s">PHP version upgrades</a>.', 'seravo'),
+        $recommended_version,
+        $url
       );
       ?>
       </p>
