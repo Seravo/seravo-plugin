9b84f1ae7762e8c37e86febefce02d399a10229f
Viljami Pietarila <viljami@seravo.com>
2020-07-21 11:31:48 +0300

    Remove special characters on upload
    Replace characters like ä with a and ö with o when a file
    with such characters is uploaded.
    
    Add a toggle to opt-in for this feature in 'Site status'.
    By default it is off.
    
    Also add the small fix to browser validate optimize images
    size.
    
    Closes #71, #415

---
+++ modules/sanitize-on-upload.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Sanitize special characters out from file names on upload.
+ */
+
+namespace Seravo;
+
+// Deny direct access to this file
+if ( ! defined('ABSPATH') ) {
+  die('Access denied!');
+}
+
+if ( ! class_exists('SanitizeOnUpload') ) {
+
+    class SanitizeOnUpload {
+
+    public static function load() {
+        add_filter('wp_handle_upload_prefilter', array( __CLASS__, 'upload_prefilter' ), 10, 1);
+    }
+
+    public static function upload_prefilter( $file ) {
+        // Convert all characters in the filename to HTML entities first and then
+        // run a replacement to replace them with standard characters.
+        $file['name'] = preg_replace('/&([a-z])[a-z]+;/i', '$1', htmlentities($file['name']));
+        return $file;
+    }
+
+    }
+    if ( get_option('seravo-enable-sanitize-uploads') === 'on' ) {
+        SanitizeOnUpload::load();
+    }
+}
