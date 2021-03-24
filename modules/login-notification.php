09d6f5591e794196a39c4c377263efb41ac033cf
Santeri Kangas <santeri@seravo.com>
2021-03-24 16:20:09 +0200

    Add dashboard widget to show site status (Closes: #5)

--- modules/login-notification.php
+++ modules/login-notification.php
@@ -17,6 +17,7 @@ if ( ! class_exists('Login_Notifications') ) {
 
     private static $login;
     private static $errors;
+
     // How many rows will be retrieved when reading logs, affects dashboard load time
     private static $max_rows = 200;
 
@@ -50,18 +51,16 @@ if ( ! class_exists('Login_Notifications') ) {
           self::$errors = self::retrieve_error_count();
         }
 
-        if ( self::$errors > 0 ) {
-          add_action(
-            'wp_dashboard_setup',
-            function() {
-              wp_add_dashboard_widget(
-                'seravo-error-widget',
-                __('Site Error Count', 'seravo'),
-                array( __CLASS__, 'display_admin_errors_notification' )
-              );
-            }
-          );
-        }
+        add_action(
+          'wp_dashboard_setup',
+          function() {
+            wp_add_dashboard_widget(
+              'seravo-site-widget',
+              __('Site Status', 'seravo'),
+              array( __CLASS__, 'display_site_status' )
+            );
+          }
+        );
       }
     }
 
@@ -105,7 +104,7 @@ if ( ! class_exists('Login_Notifications') ) {
             );
           }
         );
-      } else if ( $log_read['status'] === 'BAD_LOG_FILE' ) {
+      } elseif ( $log_read['status'] === 'BAD_LOG_FILE' ) {
         add_action(
           'wp_dashboard_setup',
           function() {
@@ -117,7 +116,7 @@ if ( ! class_exists('Login_Notifications') ) {
           }
         );
         return 0;
-      } else if ( $log_read['status'] === 'NO_LOG_FILE' ) {
+      } elseif ( $log_read['status'] === 'NO_LOG_FILE' ) {
         return 0;
       }
 
@@ -235,7 +234,7 @@ if ( ! class_exists('Login_Notifications') ) {
     * Display the amount of php-error.log lines that have appeared this week.
     */
     public static function display_admin_errors_notification() {
-      $url = '<a href="' . get_option('siteurl') . '/wp-admin/tools.php?page=logs_page&logfile=php-error.log">php-error.log</a>';
+      $url = '<a href="' . get_option('siteurl') . '/wp-admin/tools.php?page=logs_page&logfile=php-error.log" target="_blank">php-error.log</a>';
       $msg = wp_sprintf(
         /* translators:
         * %1$s number of errors in the log
@@ -246,6 +245,127 @@ if ( ! class_exists('Login_Notifications') ) {
         $url
       );
       echo '<div>' . $msg . '</div>';
+    }
+
+    public static function display_site_status() {
+      if ( ! Helpers::is_production() ) {
+        return _e('This feature is available only on live production sites.', 'seravo');
+      }
+      $site_info = API::get_site_data();
+      $http_requests_limit = 0;
+      echo '<h3>' . __('Plan details', 'seravo') . '</h3>';
+
+      if ( is_wp_error($site_info) ) {
+        error_log($site_info->get_error_message());
+        $url = '<a href="' . get_option('siteurl') . '/wp-admin/tools.php?page=logs_page&logfile=php-error.log" target="_blank">php-error.log</a>';
+        $error_msg = wp_sprintf(
+        // translators: %1$s url for additional information
+          __('Error on fetching plan details. See more from %1$s.', 'seravo'),
+          $url
+        );
+        echo $error_msg;
+      } else {
+        $http_requests_limit = $site_info['plan']['httplimit'];
+        echo '<div>' . __('Plan type: ', 'seravo') . $site_info['plan']['type'] . '</div>';
+        echo '<div>' . __('HTTP requests / month: ', 'seravo') . $http_requests_limit . '</div><br>';
+      }
+      // logic ported to PHP from sitestatus page
+      ?>
+      <div>
+      <h3><?php _e('Monthly HTTP requests', 'seravo'); ?></h3>
+      <?php
+      $reports = glob('/data/slog/html/goaccess-*.html');
+
+      if ( count($reports) !== 0 ) {
+        $contact_email_url = '<a href="mailto:help@seravo.com">help@seravo.com</a>';
+        $msg = wp_sprintf(
+        // translators: %1$s contact email link
+          __(
+            'These monthly reports are generated from the HTTP access logs of your site. If you have more HTTP requests than your plan allows, contact %1$s to upgrade your <a href="https://seravo.com/plans" target="_blank">plan</a>.',
+            'seravo'
+          ),
+          $contact_email_url
+        );
+        echo $msg;
+        ?>
+        </div>
+        <div class="http-requests_info_loading" style="padding: 0px;">
+          <table class="widefat striped" style="width: 100%; border: none;">
+            <thead>
+            <tr>
+              <th style="width: 25%;"><?php _e('Month', 'seravo'); ?></th>
+              <th style="width: 50%;"><?php _e('HTTP Requests', 'seravo'); ?></th>
+              <th style="width: 25%;"><?php _e('Report', 'seravo'); ?></th>
+            </tr>
+            </thead>
+        <?php
+        // Track max request value to calculate relative bar widths
+        $max_requests = 0;
+        $report_month_counter = 0;
+        $months = array();
+
+        foreach ( $reports as $report ) {
+          if ( $report_month_counter === 4 ) {
+            // show only limited amount of monthly http requests
+            break;
+          }
+          $report_month_counter++;
+          $total_requests_string = exec("grep -oE 'total_requests\": ([0-9]+),' $report");
+          preg_match('/([0-9]+)/', $total_requests_string, $total_requests_match);
+          $total_requests = intval($total_requests_match[1]);
+          if ( $total_requests > $max_requests ) {
+            $max_requests = $total_requests;
+          }
+
+          array_push(
+            $months,
+            array(
+              'month' => substr($report, 25, 7),
+              'requests' => $total_requests,
+            )
+          );
+        }
+
+        foreach ( array_reverse($months) as $month ) {
+          $month_date = $month['month'];
+          $total_requests = $month['requests'];
+          $bar_css = '';
+
+          if ( $max_requests > 0 ) {
+            $bar_size = $total_requests / $max_requests * 100;
+          } else {
+            $bar_size = 1;
+          }
+
+          if ( $bar_size <= 10 ) {
+            $bar_css = 'auto';
+          } else {
+            $bar_css = strval($bar_size) . '%';
+          }
+          $bar_border_color = 'none';
+
+          if ( $http_requests_limit != 0 && $total_requests > $http_requests_limit ) {
+            // display warning
+            $bar_border_color = 'solid 0.5em #e8ba1b';
+          }
+          // generate table element of this data
+          echo '<tr><td><a href="tools.php?x-accel-redirect&report=' . $month_date . '.html" target="_blank"> ' .
+          $month_date . ' </a> </td> <td><div style="background: #44A1CB; border-right: ' . $bar_border_color .
+          '; color: #fff; padding: 3px; width: ' . $bar_css . '; display: inline-block;"><div style="white-space: nowrap;">' .
+          $total_requests . '</div></div></td> <td><a href="tools.php?x-accel-redirect&report=' . $month_date .
+          '.html" target="_blank" class="button hideMobile">' . __('View report', 'seravo') .
+          '<span aria-hidden="true" class="dashicons dashicons-external" style="line-height: 1.4; padding-left: 3px;"></span></a></td></tr>';
+        }
+        echo '</table></div>';
+      } else {
+        _e('The site has no HTTP requests statistics yet.', 'seravo');
+        echo '</div><br>';
+      }
+
+      if ( self::$errors > 0 ) {
+        echo '<h3>' . __('PHP error status', 'seravo') . '</h3>';
+        self::display_admin_errors_notification();
+      }
     }
 
     /**
