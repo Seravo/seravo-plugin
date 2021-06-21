63f18a8effc9080a024b06dc03855b4f0230aab2
Joosua Koskinen <joosua@seravo.com>
2021-06-21 17:31:00 +0300

    Upgrade backups page to new design

--- src/modules/backups.php
+++ src/modules/backups.php
@@ -1,13 +1,13 @@
 <?php
-/*
- * Plugin name: Backups
- * Description: Enable users to list and create backups
- * Version: 1.0
+/**
+ * File for backups page.
  */
 
 namespace Seravo;
 
-require_once SERAVO_PLUGIN_SRC . 'lib/backups-ajax.php';
+use \Seravo\Postbox;
+use \Seravo\Postbox\Toolpage;
+use \Seravo\Postbox\Requirements;
 
 // Deny direct access to this file
 if ( ! defined('ABSPATH') ) {
@@ -18,110 +18,57 @@ if ( ! class_exists('Backups') ) {
   class Backups {
 
     public static function load() {
-      // Add AJAX endpoint for backups
-      add_action('wp_ajax_seravo_backups', 'Seravo\seravo_ajax_backups');
+      $page = new Toolpage('tools_page_backups_page');
 
-      add_action('admin_enqueue_scripts', array( __CLASS__, 'register_backups_scripts' ));
+      self::init_backups_postboxes($page);
 
-      // TODO: check if this hook actually ever fires for mu-plugins
-      register_activation_hook(__FILE__, array( __CLASS__, 'register_view_backups_capability' ));
-
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'backups-info',
-        __('Backups', 'seravo'),
-        array( __CLASS__, 'backups_info_postbox' ),
-        'tools_page_backups_page',
-        'normal'
-      );
-
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'backups-create',
-        __('Create a New Backup', 'seravo'),
-        array( __CLASS__, 'backups_create_postbox' ),
-        'tools_page_backups_page',
-        'normal'
-      );
-
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'backups-excludes',
-        __('Files Excluded from the Backups', 'seravo'),
-        array( __CLASS__, 'backups_excludes_postbox' ),
-        'tools_page_backups_page',
-        'side'
-      );
-
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'backups-list',
-        __('Current Backups', 'seravo'),
-        array( __CLASS__, 'backups_list_postbox' ),
-        'tools_page_backups_page',
-        'side'
-      );
+      $page->enable_ajax();
+      $page->register_page();
     }
 
-    /**
-     * Register scripts
-     *
-     * @param string $page hook name
-     */
-    public static function register_backups_scripts( $page ) {
-      wp_register_script('seravo_backups', SERAVO_PLUGIN_URL . 'js/backups.js', '', Helpers::seravo_plugin_version());
-      wp_register_style('seravo_backups', SERAVO_PLUGIN_URL . 'style/backups.css', '', Helpers::seravo_plugin_version());
+    public static function init_backups_postboxes( Toolpage $page ) {
+      /**
+       * Backup info postbox
+       */
+      $backups_info = new Postbox\InfoBox('backups-info');
+      $backups_info->set_title(__('Backups', 'seravo'));
+      $backups_info->add_paragraph(__('Backups are automatically created every night and preserved for 30 days. The data can be accessed on the server in under <code>/data/backups</code>.', 'seravo'));
+      $backups_info->set_requirements(array( Requirements::CAN_BE_ANY_ENV => true ));
+      $page->register_postbox($backups_info);
 
-      if ( $page === 'tools_page_backups_page' ) {
-        wp_enqueue_script('seravo_backups');
-        wp_enqueue_style('seravo_backups');
+      /**
+       * Backup create postbox
+       */
+      $backups_create = new Postbox\ButtonCommand('backups-create');
+      $backups_create->set_title(__('Create a New Backup', 'seravo'));
+      $backups_create->set_command('wp-backup 2>&1', 0);
+      $backups_create->set_button_text(__('Create a backup', 'seravo'));
+      $backups_create->add_paragraph(__('You can also create backups manually by running <code>wp-backup</code> on the command line. We recommend that you get familiar with the command line option that is accessible to you via SSH. That way recovering a backup will be possible whether the WP Admin is accessible or not.', 'seravo'));
+      $backups_create->set_requirements(array( Requirements::CAN_BE_ANY_ENV => true ));
+      $page->register_postbox($backups_create);
 
-        $loc_translation_backups = array(
-          'ajaxurl'    => admin_url('admin-ajax.php'),
-          'ajax_nonce' => wp_create_nonce('seravo_backups'),
-          'no_entries' => __('No entries were found', 'seravo'),
-        );
-        wp_localize_script('seravo_backups', 'seravo_backups_loc', $loc_translation_backups);
-      }
+      /**
+       * Backup excludes postbox
+       */
+      $backups_excludes = new Postbox\AutoCommand('backups-excludes', 'side');
+      $backups_excludes->set_title(__('Files Excluded from the Backups', 'seravo'));
+      $backups_excludes->set_command('cat /data/backups/exclude.filelist', 60, true);
+      $backups_excludes->add_paragraph(__('Below are the contents of <code>/data/backups/exclude.filelist</code>.', 'seravo'));
+      $backups_excludes->set_empty_message(__('No files are excluded', 'seravo'));
+      $backups_excludes->set_requirements(array( Requirements::CAN_BE_ANY_ENV => true ));
+      $page->register_postbox($backups_excludes);
 
+      /**
+       * Current Backups postbox
+       */
+      $backups_list = new Postbox\AutoCommand('backups-list-', 'side');
+      $backups_list->set_title(__('Current Backups', 'seravo'));
+      $backups_list->set_command('wp-backup-status 2>&1', 300);
+      $backups_list->add_paragraph(__('This list is produced by the command <code>wp-backup-status</code>.', 'seravo'));
+      $backups_list->set_requirements(array( Requirements::CAN_BE_ANY_ENV => true ));
+      $page->register_postbox($backups_list);
     }
 
-    public static function backups_info_postbox() {
-      ?>
-      <p><?php _e('Backups are automatically created every night and preserved for 30 days. The data can be accessed on the server in under <code>/data/backups</code>.', 'seravo'); ?></p>
-      <?php
-    }
-
-    public static function backups_create_postbox() {
-      ?>
-      <p><?php _e('You can also create backups manually by running <code>wp-backup</code> on the command line. We recommend that you get familiar with the command line option that is accessible to you via SSH. That way recovering a backup will be possible whether the WP Admin is accessible or not.', 'seravo'); ?></p>
-      <p class="create_backup">
-        <button id="create_backup_button" class="button-primary"><?php _e('Create a backup', 'seravo'); ?> </button>
-        <div id="create_backup_loading"><img class="hidden" src="/wp-admin/images/spinner.gif"></div>
-        <pre><div id="create_backup"></div></pre>
-      </p>
-      <?php
-    }
-
-    public static function backups_excludes_postbox() {
-      // translators: %s name of the file shown
-      printf(__('Below are the contents of %s.', 'seravo'), '<code>/data/backups/exclude.filelist</code>');
-      ?>
-      <p>
-        <div id="backup_exclude_loading">
-          <img src="/wp-admin/images/spinner.gif">
-        </div>
-        <pre id="backup_exclude"></pre>
-      </p>
-      <?php
-    }
-
-    public static function backups_list_postbox() {
-      // translators: %s command used to list WordPress backups of the website
-      printf(__('This list is produced by the command %s.', 'seravo'), '<code>wp-backup-status</code>');
-      ?>
-      <p>
-        <div id="backup_status_loading"><img src="/wp-admin/images/spinner.gif"></div>
-        <pre id="backup_status"></pre>
-      </p>
-      <?php
-    }
   }
 
   Backups::load();
