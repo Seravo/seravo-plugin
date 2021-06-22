9f1e8a4c279eb4b899df4407e445315721b7446d
Santeri Kangas <santeri@seravo.com>
2021-06-22 15:22:36 +0300

    Rewrite database page widgets (Related: 544)
    - Database access
    - Adminer link & info
    - Database cleanup tools
    - Misc changes/additions to helpers

--- src/modules/database.php
+++ src/modules/database.php
@@ -7,6 +7,13 @@
 
 namespace Seravo;
 
+use Seravo\Ajax\ButtonCommand;
+use \Seravo\Postbox;
+use Seravo\Postbox\Component;
+use \Seravo\Postbox\Template;
+use \Seravo\Postbox\Toolpage;
+use \Seravo\Postbox\Requirements;
+
 // Deny direct access to this file
 if ( ! defined('ABSPATH') ) {
   die('Access denied!');
@@ -35,53 +42,68 @@ if ( ! class_exists('Database') ) {
         $GLOBALS['sr_alltables'] = false;
       }
 
+      $page = new Toolpage('tools_page_database_page');
+      self::init_database_postboxes($page);
+
+      $page->enable_ajax();
+      $page->register_page();
+
       add_action('admin_enqueue_scripts', array( __CLASS__, 'enqueue_database_scripts' ));
 
       // Add AJAX endpoints for wp search-replace, database info and database cleanup
       add_action('wp_ajax_seravo_search_replace', 'Seravo\seravo_ajax_search_replace');
       add_action('wp_ajax_seravo_wp_db_info', 'Seravo\seravo_ajax_get_wp_db_info');
-      add_action('wp_ajax_seravo_db_cleanup', 'Seravo\seravo_ajax_db_cleanup');
+    }
 
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'database-access',
-        __('Database Access', 'seravo'),
-        array( __CLASS__, 'database_access_postbox' ),
-        'tools_page_database_page',
-        'normal'
-      );
+    public static function init_database_postboxes( Toolpage $page ) {
+      /**
+       * Database access info postbox
+       */
+      $database_access = new Postbox\InfoBox('database-access');
+      $database_access->set_title(__('Database Access', 'seravo'));
+      $database_access->set_requirements(array( Requirements::CAN_BE_PRODUCTION => true ));
+      $database_access->add_paragraph(__('You can find the database credentials by connecting to your site with SSH and running the command <code>wp-list-env</code>. These credentials can be used to connect to the server with an SSH tunnel. You can also use the web-based Adminer available on this page.', 'seravo'));
+      $database_access->add_paragraph(__('When you have established an SSH connection you can use WP-CLI that features powerful database tools for example exports and imports. <a href="https://developer.wordpress.org/cli/commands/db/" target="_BLANK">Read the documentation for wp db</a>.', 'seravo'));
+      $page->register_postbox($database_access);
 
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'database-adminer',
-        __('Manage the Database with Adminer', 'seravo'),
-        array( __CLASS__, 'database_adminer_postbox' ),
-        'tools_page_database_page',
-        'normal'
-      );
+      /**
+       * Database adminer info postbox
+       */
+      $adminer = new Postbox\Postbox('database-adminer');
+      $adminer->set_title(__('Manage the Database with Adminer', 'seravo'));
+      $adminer->set_requirements(array( Requirements::CAN_BE_PRODUCTION => true ));
+      $adminer->set_build_func(array( __CLASS__, 'build_adminer_postbox' ));
+      $page->register_postbox($adminer);
 
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'database-search-replace',
-        __('Search-Replace Tool', 'seravo'),
-        array( __CLASS__, 'database_search_replace_postbox' ),
-        'tools_page_database_page',
-        'side'
-      );
+      /**
+       * Search & Replace tool postbox
+       * To be implemented...
+       */
+      $search_replace = new Postbox\Postbox('database-search-replace');
+      $search_replace->set_title(__('Search-Replace Tool', 'seravo'));
+      $search_replace->set_requirements(array( Requirements::CAN_BE_PRODUCTION => true ));
+      $search_replace->set_build_func(array( __CLASS__, 'database_search_replace_postbox' ));
+      $page->register_postbox($search_replace);
 
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'database-cleanup',
-        __('Database Cleanup Tool', 'seravo'),
-        array( __CLASS__, 'database_cleanup_postbox' ),
-        'tools_page_database_page',
-        'side'
-      );
+      /**
+       * Database cleanup tool postbox
+       */
+      $cleanup = new Postbox\Postbox('database-cleanup');
+      $cleanup->set_title(__('Database Cleanup Tool', 'seravo'));
+      $cleanup->set_requirements(array( Requirements::CAN_BE_PRODUCTION => true ));
+      $cleanup->set_build_func(array( __CLASS__, 'build_database_cleanup' ));
+      self::init_cleanup_ajax_scripts($cleanup);
+      $page->register_postbox($cleanup);
 
-      \Seravo\Postbox\seravo_add_raw_postbox(
-        'database-size',
-        __('Database Size', 'seravo'),
-        array( __CLASS__, 'database_size_postbox' ),
-        'tools_page_database_page',
-        'side'
-      );
-
+      /**
+       * Database size postbox
+       * To be implemented...
+       */
+      $size = new Postbox\Postbox('database-size');
+      $size->set_title(__('Database Size', 'seravo'));
+      $size->set_requirements(array( Requirements::CAN_BE_PRODUCTION => true ));
+      $size->set_build_func(array( __CLASS__, 'database_size_postbox' ));
+      $page->register_postbox($size);
     }
 
     /**
@@ -110,77 +132,61 @@ if ( ! class_exists('Database') ) {
 
     }
 
-    public static function database_access_postbox() {
-      ?>
-      <p>
-        <?php
-        // translators: $s example of the command for getting user's database credentials
-        printf(__('You can find the database credentials by connecting to your site with SSH and running the command %s. These credentials can be used to connect to the server with an SSH tunnel. You can also use the web-based Adminer available on this page.', 'seravo'), '<code>wp-list-env</code>');
-        ?>
-      </p>
-      <p>
-        <?php
-        // translators: $s url containing additional information on WordPress database tools
-        printf(__('When you have established an SSH connection you can use WP-CLI that features powerful database tools for example exports and imports. <a href="%s" target="_BLANK">Read the documentation for wp db</a>.', 'seravo'), 'https://developer.wordpress.org/cli/commands/db/');
-        ?>
-      </p>
-      <?php
+    /**
+     * Helper method for initializing cleanup & optimize postbox tools.
+     * @param Postbox $cleanup Postbox on which the commands and ajax are initialized.
+     */
+    public static function init_cleanup_ajax_scripts( Postbox\Postbox $cleanup ) {
+      // Initialize optimize section
+      $optimize = new ButtonCommand('optimize-db');
+
+      // define the db credentials for wp-db-optimize
+      $db_info = defined('DB_NAME') ? 'DB_NAME=' . DB_NAME . ' ' : '';
+      $db_info .= defined('DB_HOST') ? 'DB_HOST=' . DB_HOST . ' ' : '';
+      $db_info .= defined('DB_USER') ? 'DB_USER=' . DB_USER . ' ' : '';
+      $db_info .= defined('DB_PASSWORD') ? 'DB_PASSWORD=' . DB_PASSWORD . ' ' : '';
+      $db_optimize_command = $db_info . 'wp-db-optimize 2>&1';
+
+      $optimize->set_command($db_optimize_command, 0, true);
+      $optimize->set_button_text(__('Optimize', 'seravo'));
+
+      // Initialize cleanup section
+      $cleanup_command = new ButtonCommand('cleanup-db');
+      $cleanup_command->set_command('wp-backup && wp-db-cleanup 2>&1', 0, false);
+      $cleanup_command->set_dryrun_command('wp-db-cleanup --dry-run');
+      $cleanup_command->set_button_text(__('Run wp-db-cleanup', 'seravo'), __('Do a dry run', 'seravo'));
+      $cleanup_command->set_empty_message(__('Nothing to be cleaned up', 'seravo'));
+
+      $cleanup->add_ajax_handler($optimize);
+      $cleanup->add_ajax_handler($cleanup_command);
     }
 
-    public static function database_adminer_postbox() {
-      ?>
-      <p>
-        <?php
-        /* translators:
-        * %1$s url to www.adminer.org
-        */
-        printf(__('<a href="%1$s" target="_BLANK">Adminer</a> is a visual database management tool, which is simpler and safer than its competitor phpMyAdmin.', 'seravo'), 'https://www.adminer.org');
-        ?>
-      </p>
-      <p>
-        <?php
-        /* translators:
-        * %1$s example url for accessing Adminer: example.com/.seravo/adminer
-        */
-        printf(__('At Seravo it can always be accessed at %1$s.', 'seravo'), '<code>example.com/.seravo/adminer</code>');
-        ?>
-      </p>
-      <?php
-      $adminer_url = '';
+    /**
+     * Build the database cleanup and optimize postbox.
+     * @param Component $base The postbox base component.
+     * @param Postbox $postbox To fetch the AJAX components from.
+     */
+    public static function build_database_cleanup( Component $base, Postbox\Postbox $postbox ) {
+      $base->add_child(Template::section_title(__('Optimization', 'seravo')));
+      $base->add_child(Template::paragraph(__('You can use this tool to run <code>wp-db-optimize</code>. The command optimizes WordPress database and thus reduces database disk usage.', 'seravo')));
+      $base->add_child($postbox->get_ajax_handler('optimize-db')->get_component());
 
-      // TODO: test for multisite
-      $siteurl = get_site_url();
+      $base->add_child(Template::section_title(__('Cleanup', 'seravo')));
+      $base->add_child(Template::paragraph(__('You can use this tool to run <code>wp-db-cleanup</code>. For safety reason a dry run is compulsory before the actual cleanup can be done.', 'seravo')));
+      $base->add_child($postbox->get_ajax_handler('cleanup-db')->get_component());
+    }
 
-      if ( 'production' === getenv('WP_ENV') ) {
+    /**
+     * Build the Adminer info postbox.
+     * @param Component $base The base component to add content.
+     */
+    public static function build_adminer_postbox( Component $base ) {
+      $base->add_child(Template::paragraph(__('<a href="https://www.adminer.org" target="_BLANK">Adminer</a> is a visual database management tool, which is simpler and safer than its competitor phpMyAdmin.', 'seravo')));
+      $base->add_child(Template::paragraph(__('At Seravo it can always be accessed at <code>example.com/.seravo/adminer</code>', 'seravo')));
 
-        // Add trailing slash if missing
-        if ( substr($siteurl, -1) !== '/' ) {
-          $siteurl .= '/';
-        }
-
-        $adminer_url = $siteurl . '.seravo/adminer';
-
-      } else {
-
-        // Add trailing slash if missing
-        if ( substr($siteurl, -1) !== '/' ) {
-          $siteurl .= '/';
-        }
-
-        // Inject subdomain
-        $adminer_url = str_replace('//', '//adminer.', $siteurl);
-
-      }
-
-      ?>
-
-      <p class="adminer_button">
-        <a href="<?php echo esc_url($adminer_url); ?>" class="button" target="_blank">
-          <?php _e('Open Adminer', 'seravo'); ?>
-          <span aria-hidden="true" class="dashicons dashicons-external" style="line-height: 1.4; padding-left: 3px;"></span>
-        </a>
-      </p>
-      <?php
+      $button = Template::button_link_with_icon(Helpers::adminer_link(), __('Open Adminer', 'seravo'));
+      $button->set_wrapper('<p class="adminer_button">', '</p>');
+      $base->add_child($button);
     }
 
     public static function database_search_replace_postbox() {
@@ -220,36 +226,6 @@ if ( ! class_exists('Database') ) {
         <table id="search_replace"></table>
         <?php
       endif;
-    }
-
-    public static function database_cleanup_postbox() {
-      ?>
-      <h3> <?php _e('Optimization', 'seravo'); ?> </h3>
-      <p> 
-      <?php
-      _e(
-        'You can use this tool to run <code>wp-db-optimize</code>. The command optimizes WordPress database and thus
-      reduces database disk usage.',
-        'seravo'
-      );
-      ?>
-      </p>
-
-      <button id='optimize-button' class='button optimize'><?php _e('Optimize', 'seravo'); ?> </button>
-      <div id='optimize_loading'><img class="hidden" src="/wp-admin/images/spinner.gif"></div>
-      <div id='db_optimize'></div>
-    
-      <br>
-      <h3> <?php _e('Cleanup', 'seravo'); ?> </h3>
-      <p> <?php _e('You can use this tool to run <code>wp-db-cleanup</code>. For safety reason a dry run is compulsory before the actual cleanup can be done.', 'seravo'); ?></p>
-      <div class="datab_buttons">
-            <button id="cleanup-drybutton" class="button cleanup-button"> <?php _e('Do a dry run', 'seravo'); ?> </button>
-            <button id="cleanup-button" class="button cleanup-button" disabled> <?php _e('Run wp-db-cleanup', 'seravo'); ?> </button>
-          </div>
-        <div id="cleanup_loading"><img class="hidden" src="/wp-admin/images/spinner.gif"></div>
-        <div id="cleanup_command"></div>
-        <table id="db_cleanup"></table>
-      <?php
     }
 
     public static function database_size_postbox() {
