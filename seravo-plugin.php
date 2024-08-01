7bca767e0bfbf75558389c4dda6848e22b9385f6
Ronnie Friman <ronnie@seravo.com>
2024-08-01 15:53:26 +0300

    Bump version to 1.10.1

--- seravo-plugin.php
+++ seravo-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Seravo Plugin
- * Version: 1.10.0
+ * Version: 1.10.1
  * Plugin URI: https://github.com/Seravo/seravo-plugin
  * Description: Enhances WordPress with Seravo.com specific features and integrations.
  * Author: Seravo Oy
